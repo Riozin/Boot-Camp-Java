a308469cbe1e450b3f992eaf62b235a79fbcb88b
Samuel Rios <rioss200512345@gmail.com>
2025-03-20 19:51:32 -0300

    Aula Tipos e Variaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,28 @@
+public class TiposVariaveis {
+
+
+    public static void main(String[] args) {
+        
+        //tipos primitivos
+        //estudem a classe Sting que representa texto na aplicacao
+
+
+        
+        double salarioMinimo = 2500;
+        long cpf = 54722569843L;
+        float pi = 3.14F;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        //short numeroCurto2 = numeroNormal; // Erro 
+
+        int numero = 5; //variavel comum
+        numero = 12; // pode ter seu valor alterado depois sem probelmas
+
+        final double PI = 3.14;
+        //PI = 3.12; erro, constantes não podem ter valor alterado depois de declarada
+
+
+
+    }
+}
