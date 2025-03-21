85f5ae97ee46d39899379f9829235758fb613dda
Samuel Rios <rioss200512345@gmail.com>
2025-03-21 08:37:00 -0300

    Aula Operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,69 @@
+
+
+public class Operadores {
+    public static void main(String[] args) {
+        
+
+        // String nome = "Samuel";
+        // int idade = 10;
+        // double peso = 60.5;
+        // boolean doadorOrgao = false;
+
+        // //Operadores aritimeticos
+
+        // double soma = 10.5 + 14.5;
+        // int subtração = 112 - 124;
+        // int multiplicação = 12 * 12;
+        // int divisao = 12/12;
+        // int modulo = 18 % 3;
+        // double resultado = (10 * 6) + (12 + 12);
+
+        //Operadores unarios
+
+        // + operador unario de valor positivo
+        // - operador unario de valor negativo
+        // ++ operador unario de incremento
+        // -- operador de decremento
+        // ! operador de negacao  
+
+
+        // int numero = 5;
+        // numero+= -1;
+        // numero++;
+        // System.out.println(-numero);
+
+        //Operador ternario
+        //Uma condição if else escrita em apenas uma linha
+
+        // int mediaFinal = 5;
+
+        // boolean aprovado = (mediaFinal > 5) ? true: false;
+        // System.out.println(aprovado);
+
+        // int idade = 19;
+        // String maiorIdade = idade >=18 ? "SIM": "NÃO";
+        // System.out.println(maiorIdade);
+
+        //Operadores relacionais, tem a função de avaliar a relação de duas variaveis
+
+
+        // == compara se são iguais
+        // != compara se sao diferentes
+        // > maior
+        // >= maior ou igual
+        // < menor
+        // <= menor ou igual
+
+        //Operadores lógicos
+
+        //Representam expressõe lógicas
+
+        // logica &&   E
+        // logica ||   OU
+        // logica !    NOT/NAO
+        
+
+
+
+    }
+}
